a5d17dc4372c194972766d10157385ee75370049
Vaibhav <vaibhavkangane1424@gmail.com>
2026-01-12 14:33:43 +0530

    now i created structure of my folder

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,4 @@
+function Login() {
+  return <h2>Login Page Working</h2>;
+}
+export default Login;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,4 @@
+function Register() {
+  return <h2>Register Page Working</h2>;
+}
+export default Register;
